3a7d10323405dbbbab4996e5b790ebb531d5a407
iamsahan <weerakkodyse@gmail.com>
2025-06-11 09:32:29 +0530

    refactor lead controller

--- src/main/java/com/example/crmchatbotbackend/controller/LeadController.java
+++ src/main/java/com/example/crmchatbotbackend/controller/LeadController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Map;                      // <--- Add this import
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
